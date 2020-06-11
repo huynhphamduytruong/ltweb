35558f3d00f22ec53c99a76b44fca7f5c97664ba
minh8250vn <minh8250vn@gmail.com>
2020-06-11 21:12:27 +0700

    News commit

---
+++ Migrations/202006111354028_CreateTableNews.cs
@@ -0,0 +1,39 @@
+namespace ltweb.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateTableNews : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.News",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Description = c.String(),
+                        DateTime = c.DateTime(nullable: false),
+                        CategoryId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.News");
+            DropTable("dbo.Categories");
+        }
+    }
+}
