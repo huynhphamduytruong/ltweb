ef2622a3fc6598bc0ffcb3bfab7a73e5c2b74314
minh8250vn <minh8250vn@gmail.com>
2020-06-11 21:24:30 +0700

    Region commit

--- Models/News.cs
+++ Models/News.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,20 @@ namespace ltweb.Models
     public class News
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public DateTime DateTime { get; set; }
-        public  Category Category { get; set; } 
+        public  Category Category { get; set; }
+        [Required]
         public int CategoryId { get; set; }
+        public Region Region { get; set; }
+        [Required]
+        public int RegionId { get; set; }
+        [Required]
+        public int UserId { get; set; }
         
     }
 }
